d98dadc47c922859e2311c1326db7fe44acfc54c
thorion21 <38396004+thorion21@users.noreply.github.com>
2020-11-01 19:13:42 +0100

    Added ML code

---
+++ 2nhackML/train_model.py
@@ -0,0 +1,173 @@
+import matplotlib
+matplotlib.use("Agg")
+import gc
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import LearningRateScheduler
+from keras.optimizers import Adagrad
+from keras.utils import np_utils
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from pyimagesearch.hacknet import HackNet
+from pyimagesearch import config
+from imutils import paths
+from keras.utils.multi_gpu_utils import multi_gpu_model
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import os
+import tensorflow as tf
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+NUM_EPOCHS = 40
+INIT_LR = 1e-3
+BS = 32
+
+trainPaths = list(paths.list_images(config.TRAIN_PATH))
+totalTrain = len(trainPaths)
+totalVal = len(list(paths.list_images(config.VAL_PATH)))
+totalTest = len(list(paths.list_images(config.TEST_PATH)))
+
+trainLabels = [int(p.split(os.path.sep)[-2]) for p in trainPaths]
+trainLabels = np_utils.to_categorical(trainLabels)
+classTotals = trainLabels.sum(axis=0)
+classWeight = classTotals.max() / classTotals
+
+from keras.backend.tensorflow_backend import set_session
+from keras.backend.tensorflow_backend import clear_session
+from keras.backend.tensorflow_backend import get_session
+import tensorflow
+
+def reset_keras():
+    sess = get_session()
+    clear_session()
+    sess.close()
+    sess = get_session()
+
+    try:
+        del model
+    except:
+        pass
+
+    print(gc.collect())
+
+    # use the same config as you used to create the session
+    config = tensorflow.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = 1
+    config.gpu_options.visible_device_list = "0,1"
+    set_session(tensorflow.Session(config=config))
+
+# reset_keras()
+
+# initialize the training training data augmentation object
+trainAug = ImageDataGenerator(
+	rescale=1 / 255.0,
+	rotation_range=20,
+	zoom_range=0.05,
+	width_shift_range=0.1,
+	height_shift_range=0.1,
+	shear_range=0.05,
+	horizontal_flip=True,
+	vertical_flip=True,
+	fill_mode="nearest")
+
+# initialize the validation (and testing) data augmentation object
+valAug = ImageDataGenerator(rescale=1 / 255.0)
+
+# initialize the training generator
+trainGen = trainAug.flow_from_directory(
+	config.TRAIN_PATH,
+	class_mode="categorical",
+	target_size=(48, 48),
+	color_mode="rgb",
+	shuffle=True,
+	batch_size=BS)
+
+# initialize the validation generator
+valGen = valAug.flow_from_directory(
+	config.VAL_PATH,
+	class_mode="categorical",
+	target_size=(48, 48),
+	color_mode="rgb",
+	shuffle=False,
+	batch_size=BS)
+
+# initialize the testing generator
+testGen = valAug.flow_from_directory(
+	config.TEST_PATH,
+	class_mode="categorical",
+	target_size=(48, 48),
+	color_mode="rgb",
+	shuffle=False,
+	batch_size=BS)
+
+IMAGE_SIZE = 48
+# available GPUs
+G = 1
+print("[INFO] training with {} GPUs...".format(G))
+
+# we'll store a copy of the model on *every* GPU and then combine
+# the results from the gradient updates on the CPU
+with tf.device("/cpu:0"):
+    model = HackNet.build(width=IMAGE_SIZE, height=IMAGE_SIZE, depth=3, classes=2)
+
+# model = multi_gpu_model(model, gpus=G)
+opt = Adagrad(lr=INIT_LR, decay=INIT_LR / NUM_EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# fit the model
+H = model.fit_generator(
+	trainGen,
+	steps_per_epoch=totalTrain // BS,
+	validation_data=valGen,
+	validation_steps=totalVal // BS,
+	class_weight=classWeight,
+	epochs=NUM_EPOCHS,
+	use_multiprocessing=True,
+	workers=30)
+
+
+print("[INFO] evaluating network...")
+testGen.reset()
+predIdxs = model.predict_generator(testGen,
+	steps=(totalTest // BS) + 1)
+
+# for each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
+
+model.save('hacknet.h5')
+
+print(classification_report(testGen.classes, predIdxs,
+	target_names=testGen.class_indices.keys()))
+
+cm = confusion_matrix(testGen.classes, predIdxs)
+total = sum(sum(cm))
+acc = (cm[0, 0] + cm[1, 1]) / total
+sensitivity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
+specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
+
+# show the confusion matrix, accuracy, sensitivity, and specificity
+print(cm)
+print("acc: {:.4f}".format(acc))
+print("sensitivity: {:.4f}".format(sensitivity))
+print("specificity: {:.4f}".format(specificity))
+
+# plot the training loss and accuracy
+N = NUM_EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_accuracy")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_accuracy")
+plt.title("Training Loss and Accuracy on Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
